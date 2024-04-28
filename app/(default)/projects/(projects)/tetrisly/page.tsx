96e670e7346d86a990372c99e6e4e95af5d2ec25
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2024-04-28 20:52:36 +0200

    feat: add tetrisly what I learned

--- app/(default)/projects/(projects)/tetrisly/page.tsx
+++ app/(default)/projects/(projects)/tetrisly/page.tsx
@@ -12,6 +12,8 @@ import { ScreenWrapper } from "@/app/(default)/_components/ScreenWrapper";
 import { Hero } from "../_components/Hero";
 import { navigationConfig } from "@/config/navigationConfig";
 import { RoleElement } from "../_components/RoleElement";
+import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
 
 export default function TetrislyPage() {
   return (
@@ -204,7 +206,49 @@ export default function TetrislyPage() {
         <h2 className="lg:text-center">
           What have I learned from working on Tetrisly?
         </h2>
+        <ul className="flex flex-wrap items-center justify-center gap-4 rounded bg-slate-100 p-4">
+          <Skill>
+            The importance of a design system in a project and how it can{" "}
+            <BoldSkill>speed up</BoldSkill> the development process
+          </Skill>
+          <Skill>
+            Good practices of <BoldSkill>UX/UI design</BoldSkill>
+          </Skill>
+          <Skill>
+            How to use <BoldSkill>Figma</BoldSkill> efficiently
+          </Skill>
+          <Skill>
+            How the choice of <BoldSkill>styling library</BoldSkill> can impact
+            future product
+          </Skill>
+          <Skill>
+            What is the <BoldSkill>design system</BoldSkill>
+          </Skill>
+          <Skill>
+            How to <BoldSkill>test</BoldSkill> complex components in React
+          </Skill>
+          <Skill>
+            Plan correct variants of components with an apropriate{" "}
+            <BoldSkill>TypeScript support</BoldSkill>
+          </Skill>
+        </ul>
       </section>
     </>
   );
 }
+
+const BoldSkill = ({ children }: { children: ReactNode }) => (
+  <span className="font-bold text-primary">{children}</span>
+);
+
+const Skill = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => (
+  <li className={cn("max-w-sm border bg-white p-4 text-center", className)}>
+    {children}
+  </li>
+);
